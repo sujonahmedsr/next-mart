af4bfbc95bcd6bebb8b31b26e9848b22c75b1971
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-02-17 17:51:49 +0600

    authentication working

---
+++ client/src/app/(WithCommonLayout)/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+
+const HomePage = () => {
+
+  return (
+    <div>
+      <h1>Welcome To NextMart Home Page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ client/src/services/AuthService/index.ts
@@ -0,0 +1,61 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use server";
+
+import { jwtDecode } from "jwt-decode";
+import { cookies } from "next/headers";
+import { FieldValues } from "react-hook-form";
+
+export const registerUser = async (userData: FieldValues) => {
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/user`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+      const result = await res.json();
+  
+      if (result.success) {
+        (await cookies()).set("accessToken", result.data.accessToken);
+      }
+  
+      return result;
+    } catch (error: any) {
+      return Error(error);
+    }
+  };
+
+  export const loginUser = async (userData: FieldValues) => {
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/auth/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+  
+      const result = await res.json();
+  
+      if (result.success) {
+        (await cookies()).set("accessToken", result.data.accessToken);
+      }
+  
+      return result;
+    } catch (error: any) {
+      return Error(error);
+    }
+  };
+
+  export const getCurrentUser = async () => {
+    const accessToken = (await cookies()).get("accessToken")!.value;
+    let decodedData = null;
+  
+    if (accessToken) {
+      decodedData = await jwtDecode(accessToken);
+      return decodedData;
+    } else {
+      return null;
+    }
+  };
