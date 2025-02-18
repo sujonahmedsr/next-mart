5d820c66d427fc5576386e931e4fbf3336a7e710
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-02-19 01:26:57 +0600

    up

--- client/src/components/modules/dashboard/sidebar/app-sidebar.tsx
+++ client/src/components/modules/dashboard/sidebar/app-sidebar.tsx
@@ -99,7 +99,7 @@ const data = {
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar variant="inset" {...props}>
+    <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
