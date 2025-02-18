3104392197189cb676980466168799a3cf00606b
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-02-19 01:23:15 +0600

    updated

--- client/src/services/Category/index.ts
+++ client/src/services/Category/index.ts
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use server";
 
+import { revalidateTag } from "next/cache";
 import { cookies } from "next/headers";
 
+// create category
 export const createCategory = async (data: FormData) => {
   try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/category`, {
@@ -13,16 +15,42 @@ export const createCategory = async (data: FormData) => {
       body: data,
     });
 
+    revalidateTag("CATEGORY");
+
     return res.json();
   } catch (error: any) {
     return Error(error);
   }
 };
 
+//get all categories
 export const getAllCategories = async () => {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/category`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/category`, {
+      next: {
+        tags: ["CATEGORY"],
+      },
+    });
 
+    return res.json();
+  } catch (error: any) {
+    return Error(error);
+  }
+};
+
+// delete category
+export const deleteCategory = async (categoryId: string): Promise<any> => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_API}/category/${categoryId}`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: (await cookies()).get("accessToken")!.value,
+        },
+      }
+    );
+    revalidateTag("CATEGORY");
     return res.json();
   } catch (error: any) {
     return Error(error);
