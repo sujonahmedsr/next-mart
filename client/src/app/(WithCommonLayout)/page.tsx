d0f4937049ee6b3997ff87a18425086dd2b2fa05
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-02-22 00:25:03 +0600

    add flash shale

--- client/src/app/(WithCommonLayout)/page.tsx
+++ client/src/app/(WithCommonLayout)/page.tsx
@@ -1,7 +1,8 @@
 import Category from "@/components/modules/home/Category";
 import FeaturedProducts from "@/components/modules/home/FeaturedProducts";
+import FlashSale from "@/components/modules/home/FlashSale";
 import HeroSection from "@/components/modules/home/HeroSection";
-import TopBrands from "@/components/modules/home/TopBrands/page";
+import TopBrands from "@/components/modules/home/TopBrands";
 
 const HomePage = () => {
   return (
@@ -9,6 +10,7 @@ const HomePage = () => {
       <HeroSection />
       <Category />
       <FeaturedProducts />
+      <FlashSale />
       <TopBrands />
     </div>
   );
---
+++ client/src/components/modules/home/FlashSale/index.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@/components/ui/button";
+import ProductCard from "@/components/ui/core/ProductCard";
+import { getFlashSaleProducts } from "@/services/FlashSale";
+import { IProduct } from "@/types/product";
+import Link from "next/link";
+
+
+const FlashSale = async () => {
+  const { data: products } = await getFlashSaleProducts();
+
+  return (
+    <div className=" bg-white bg-opacity-50 pt-6 pb-8">
+      <div className="container mx-auto px-5 my-16">
+        <div className="flex items-center justify-between ">
+          <div className="flex items-center gap-8">
+            <h2 className="text-3xl font-bold">Flash Sale</h2>
+            {/* <CountDown /> */}
+          </div>
+
+          <Link href="/products">
+            <Button variant="outline" className="rounded-full">
+              All Collection
+            </Button>
+          </Link>
+        </div>
+        <div className="grid grid-cols-4 gap-4 mt-10">
+          {products?.slice(0, 4)?.map((product: IProduct, idx: number) => (
+            <ProductCard key={idx} product={product} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FlashSale;
