71ed3ae46d4cc07accfa739b453b1cf00752b174
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-02-24 01:37:48 +0600

    some code changes

--- client/src/redux/features/cartSlice.ts
+++ client/src/redux/features/cartSlice.ts
@@ -7,11 +7,15 @@ export interface CartProduct extends IProduct {
 }
 
 interface TinitialState {
-    products: CartProduct[]
+    products: CartProduct[],
+    city: string;
+    shippingAddress: string;
 }
 
 const initialState: TinitialState = {
-    products: []
+    products: [],
+    city: '',
+    shippingAddress: ''
 }
 
 
@@ -56,25 +60,91 @@ const cartSlice = createSlice({
                 (product) => product._id !== action.payload
             );
         },
+        updateCity: (state, action) => {
+            state.city = action.payload;
+        },
+        updateShippingAddress: (state, action) => {
+            state.shippingAddress = action.payload;
+        },
+        clearCart: (state) => {
+            state.products = [];
+            state.city = "";
+            state.shippingAddress = "";
+        },
     }
 })
 
 //* Products
-
 export const orderedProductsSelector = (state: RootState) => {
     return state.cart.products;
 };
 
+// orderSelector
+export const orderSelector = (state: RootState) => {
+    return {
+        products: state.cart.products.map((product) => ({
+            product: product._id,
+            quantity: product.orderQuantity,
+            color: "White",
+        })),
+        shippingAddress: `${state.cart.shippingAddress} - ${state.cart.city}`,
+        paymentMethod: "Online",
+    }
+}
+
+// subTotalSelector
 export const subTotalSelector = (state: RootState) => {
     return state.cart.products.reduce((acc, product) => {
-        if(product.offerPrice){
+        if (product.offerPrice) {
             return acc + product.offerPrice * product.orderQuantity
-        }else{
+        } else {
             return acc + product.price * product.orderQuantity
         }
     }, 0)
 }
 
-export const { addProduct, incrementOrderQuantity, decrementOrderQuantity, removeProduct } = cartSlice.actions
+// shippingCostSelector
+export const shippingCostSelector = (state: RootState) => {
+    if (
+        state.cart.city &&
+        state.cart.city === "Dhaka" &&
+        state.cart.products.length > 0
+    ) {
+        return 60;
+    } else if (
+        state.cart.city &&
+        state.cart.city !== "Dhaka" &&
+        state.cart.products.length > 0
+    ) {
+        return 120;
+    } else {
+        return 0;
+    }
+};
+
+export const grandTotalSelector = (state: RootState) => {
+    const subTotal = subTotalSelector(state);
+    const shippingCost = shippingCostSelector(state);
+
+    return subTotal + shippingCost;
+};
+
+//* Address
+
+export const citySelector = (state: RootState) => {
+    return state.cart.city;
+};
+
+export const shippingAddressSelector = (state: RootState) => {
+    return state.cart.shippingAddress;
+};
+
+export const { addProduct,
+    incrementOrderQuantity,
+    decrementOrderQuantity,
+    removeProduct,
+    updateCity,
+    updateShippingAddress,
+    clearCart } = cartSlice.actions
 
 export default cartSlice.reducer
