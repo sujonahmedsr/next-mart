a11a29e9bc0b92ab063695349f3919c0c69577b5
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-02-25 23:10:00 +0600

    udpate

--- client/src/providers/StoreProvider.tsx
+++ client/src/providers/StoreProvider.tsx
@@ -1,26 +1,29 @@
-'use client'
-import { AppStore, makeStore } from '@/redux/store'
-import { useRef } from 'react'
-import { Provider } from 'react-redux'
-import { persistStore } from 'redux-persist'
-import { PersistGate } from 'redux-persist/integration/react'
+'use client';
+import { AppStore, makeStore } from '@/redux/store';
+import { useRef } from 'react';
+import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 
 export default function StoreProvider({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  const storeRef = useRef<AppStore>(undefined)
+  // Ensure proper type and initialization of the store
+  const storeRef = useRef<AppStore | null>(null);
   if (!storeRef.current) {
-    // Create the store instance the first time this renders
-    storeRef.current = makeStore()
+    storeRef.current = makeStore();
   }
 
-  const persistedStore = persistStore(storeRef.current);
+  // Persist store only once
+  const persistor = useRef(persistStore(storeRef.current)).current;
 
-  return <Provider store={storeRef.current}>
-    <PersistGate loading={false} persistor={persistedStore}>
-    {children}
-    </PersistGate>
-  </Provider>
+  return (
+    <Provider store={storeRef.current}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
