97f69c5c8977436f2a97cfcf38602d7dbd7a36ee
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-02-27 01:28:05 +0600

    updated some bug and fixed

--- client/src/app/(WithCommonLayout)/products/page.tsx
+++ client/src/app/(WithCommonLayout)/products/page.tsx
@@ -6,6 +6,7 @@ import { getAllCategories } from "@/services/Category";
 import { getAllProducts } from "@/services/Product";
 import { ICategory } from "@/types";
 
+
 type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 
 const AllProductsPage = async ({
@@ -21,7 +22,7 @@ const AllProductsPage = async ({
 
   return (
     <NMContainer>
-      <ProductBanner title="All Products" path="Home - Products" />
+      <ProductBanner title="All Products" path="Home -- Products" />
       <h2 className="text-xl font-bold my-5">Featured Collection </h2>
       <div className="grid grid-cols-6 gap-6">
         {categories?.slice(0, 6).map((category: ICategory, idx: number) => (
