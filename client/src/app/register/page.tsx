dfced4fbae36911e2163c97431c2712f3c830070
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-02-27 22:07:19 +0600

    done

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -2,7 +2,7 @@ import RegisterForm from "@/components/modules/auth/register/RegisterForm";
 
 const RegisterPage = () => {
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
+    <div className="h-screen w-screen flex justify-center items-center ">
       <RegisterForm />
     </div>
   );
